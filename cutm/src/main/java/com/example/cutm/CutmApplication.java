802939dce67fdb190ff078661a4101daa685d55d
aman raj <220101130057@cutm.ac.in>
2024-10-03 10:19:48 +0530

    commit

---
+++ cutm/src/main/java/com/example/cutm/CutmApplication.java
@@ -0,0 +1,13 @@
+package com.example.cutm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutmApplication.class, args);
+	}
+
+}
---
+++ cutm/src/test/java/com/example/cutm/CutmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cutm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
